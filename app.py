0bd2ed07ee5edb896a88a538b97c0a6a377d86ee
Proxy-49 <123624600+Proxy-49@users.noreply.github.com>
2026-05-11 13:29:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def standardize_image(input_path, output_path):
     img = img.convert("RGB")
 
     width, height = img.size
-    max_size = 2048
+    max_size = 800
 
     if max(width, height) > max_size:
         if width >= height:
